27a4d4d1d4ed32ca2550c8624d69d3f75a7ff321
Adrian <Adrianmj@MacBook-Pro-de-Adrian-2.local>
2015-11-06 10:21:02 +0000

    commit

---
+++ Assets/ColorSolid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSolid : MonoBehaviour
+{
+	public GameObject target; //the enemy's target
+	void Start () 
+	{
+		GetComponent<Renderer>().material.color = RandomColour();
+	} 
+	
+	private Color RandomColour()
+	{
+		float r = Random.value;
+		float g = Random.value;
+		float b = Random.value;
+		
+		return new Color(r, g, b);
+	}
+
+	void Swap() {
+		Vector3 temp = transform.position;
+		transform.position = target.transform.position;
+		target.transform.position = temp;
+	}
+	void Update(){
+		if (Input.GetKeyDown ("space"))
+			Swap ();
+		
+	}
+}
